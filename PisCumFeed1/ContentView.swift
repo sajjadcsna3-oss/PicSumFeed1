6a7e3652e0415adf9508382f9d5651c9b3543794
sajjadcsna3-oss <sajjadcsna3@gmail.com>
2026-04-20 16:32:58 +0500

    First commit

---
+++ PisCumFeed1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PisCumFeed1
+//
+//  Created by Mac Mini on 20/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PisCumFeed1/PisCumFeed1App.swift
@@ -0,0 +1,17 @@
+//
+//  PisCumFeed1App.swift
+//  PisCumFeed1
+//
+//  Created by Mac Mini on 20/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct PisCumFeed1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
